383727e4def95218172d61bc0131ac9f1a973655
Geordie-Castaneda <jordyorantes9@gmail.com>
2025-08-20 11:21:02 -0600

    New state in search

--- __manifest__.py
+++ __manifest__.py
@@ -19,7 +19,7 @@
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock'],
+    'depends': ['base', 'sale','stock'],
 
     # always loaded
     'data': [
